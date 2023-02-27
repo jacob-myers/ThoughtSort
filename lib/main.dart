313e3b2d465edd8ca7113bbcf942f83bfa95adb8
jacob-myers <jakemyers190@gmail.com>
2023-02-27 13:45:35 -0500

    Small edit to appbar

--- lib/main.dart
+++ lib/main.dart
@@ -15,6 +15,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
 
       // Pulls styling from styles.dart.
       theme: ThemeData(
