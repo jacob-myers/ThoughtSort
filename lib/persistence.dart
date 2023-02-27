e306cdd719925d29230ae660be0c3a7e9733b46f
Redempt <instantiable@gmail.com>
2023-02-27 13:27:54 -0500

    Add functions to save and load thoughts from a file

---
+++ lib/persistence.dart
@@ -0,0 +1,36 @@
+import 'dart:io';
+
+class Thought {
+  int id;
+  DateTime createdAt;
+  String contents;
+
+  Thought(this.id, this.createdAt, this.contents);
+
+  String toString() {
+    return "$id ${createdAt.millisecondsSinceEpoch} $contents";
+  }
+}
+
+Thought parseThought(String str) {
+  var split = str.split(" ");
+  var id = int.parse(split[0]);
+  var timestamp = int.parse(split[1]);
+  var date = DateTime.fromMillisecondsSinceEpoch(timestamp);
+  var thought = split.skip(2).join(" ");
+  return Thought(id, date, thought);
+}
+
+List<Thought> loadThoughts(String filename) {
+  var thoughts = [];
+  var file = File(filename);
+  var contents = file.readAsStringSync();
+  var lines = contents.split("\n");
+  return lines.map(parseThought).toList();
+}
+
+void saveThoughts(String filename, List<Thought> thoughts) {
+  var contents = thoughts.map((t) => t.toString()).join("\n");
+  var file = File(filename);
+  file.writeAsStringSync(contents);
+}
